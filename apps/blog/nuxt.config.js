4dee8e11bd6d58cc67b48b2b9b1a7b4984c72a42
tomasSlouka <tomas.slouka@gmail.com>
2020-07-23 12:50:35 +0200

    Add server port and host

--- apps/blog/nuxt.config.js
+++ apps/blog/nuxt.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     srcDir: __dirname,
-    buildDir: '.nuxt/blog'
+    buildDir: '.nuxt/blog',
+    server: {
+        port: 4000, // default: 3000
+        host: 'localhost' // default: localhost
+    }
 }
--- apps/forum/nuxt.config.js
+++ apps/forum/nuxt.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     srcDir: __dirname,
-    buildDir: '.nuxt/forum'
+    buildDir: '.nuxt/forum',
+    server: {
+        port: 3000, // default: 3000
+        host: 'localhost' // default: localhost
+    }
 }
