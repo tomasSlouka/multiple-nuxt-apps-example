3af6cc4016fe502d27f4e99e77f0ee54c76e219e
tomasSlouka <tomas.slouka@gmail.com>
2020-07-23 13:29:04 +0200

    Build

---
+++ .nuxt/forum/dist/server/pages/index.js
@@ -0,0 +1,44 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 18:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./apps/forum/pages/index.vue?vue&type=template&id=174ea403&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('h1',[_vm._ssrNode("The Forum")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./apps/forum/pages/index.vue?vue&type=template&id=174ea403&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(1);
+
+// CONCATENATED MODULE: ./apps/forum/pages/index.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "7f2a0722"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
