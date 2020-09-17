4a029dc7b94ec8ba990974faddd0bc17c9129cc5
tomasSlouka <tomas.slouka@gmail.com>
2020-09-17 14:33:24 +0200

    Update

---
+++ apps/indiequotes.upforweb.com/.nuxt/indiequotes.upforweb.com/dist/server/pages/index.js
@@ -0,0 +1,124 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 32:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(34);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(2).default
+module.exports.__inject__ = function (context) {
+  add("7265997d", content, true, context)
+};
+
+/***/ }),
+
+/***/ 33:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(32);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_vue_style_loader_index_js_ref_3_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_3_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_3_oneOf_1_2_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
+/***/ 34:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(1);
+exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+exports.push([module.i, "body{background-color:#0e2439}.container-narrow{margin-top:20px}h3{color:#fff;text-transform:uppercase;font-size:18px}h3>span{font-family:\"Caveat\",sans-serif;font-size:16px;position:relative;top:20px;left:-55px}iframe{height:420px;max-width:100%;width:100%;border:0}.footer{color:#fff;font-size:16px}a{color:#39ac37;text-decoration:none}.hr{border-bottom:1px solid #385673}button.cta{border-radius:0;font-size:20px}button.cta:hover{transform:rotate(10deg) scale(1.25);background-color:#39ac37}", ""]);
+// Exports
+module.exports = exports;
+
+
+/***/ }),
+
+/***/ 35:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./apps/indiequotes.upforweb.com/pages/index.vue?vue&type=template&id=2f2c4854&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"container-narrow grid justify-items-center gap-30"},[_vm._ssrNode("<div class=\"grid gap-60 md-col-2 align-items-center w100\"><div class=\" justify-self-center\"><h3>Indie hackers <span>&quot;Quotes&quot;</span></h3></div> <div class=\"grid\"><button class=\"cta justify-self-center\">Next Quote</button></div></div> <div class=\"hr\"></div> <iframe src=\"https://www.indiehackers.com/read/more/quotes\" title=\"W3Schools Free Online Web Tutorials\"></iframe> <div class=\"hr\"></div> <div class=\"footer text-center\">\n\t\tCreated by <a href=\"https://twitter.com/TomasSlouka\">@TomasSlouka 🧩</a> in 15 minutes, during the lunch break, for fun.<br>\n\t\tAll ❤️ should go to <a href=\"https://indiehackers.com\">indiehackers.com 👈</a></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./apps/indiequotes.upforweb.com/pages/index.vue?vue&type=template&id=2f2c4854&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./apps/indiequotes.upforweb.com/pages/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var pagesvue_type_script_lang_js_ = ({
+  methods: {
+    reload() {
+      location.reload();
+    }
+
+  }
+});
+// CONCATENATED MODULE: ./apps/indiequotes.upforweb.com/pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var indiequotes_upforweb_com_pagesvue_type_script_lang_js_ = (pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(3);
+
+// CONCATENATED MODULE: ./apps/indiequotes.upforweb.com/pages/index.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(33)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  indiequotes_upforweb_com_pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "6defb53a"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
